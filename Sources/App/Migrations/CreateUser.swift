0377b0fbe5c1c0b696fd06913d8b6240d4e65cb8
kim-jiha95 <kimjiha1112@gmail.com>
2024-10-17 13:01:04 +0900

    Add email field to users table

--- Sources/App/Migrations/CreateUser.swift
+++ Sources/App/Migrations/CreateUser.swift
@@ -12,11 +12,11 @@ struct CreateUser: AsyncMigration {
         try await database.schema("users")
             .id()
 //            .field("username", .string, .required)
-//            .field("email", .string, .required)
+            .field("email", .string, .required)
 //            .field("password_hash", .string, .required)
 //            .field("created_at", .datetime)
 //            .field("updated_at", .datetime)
-//            .unique(on: "email")
+            .unique(on: "email")
             .field("username", .string, .required)  // username 필드 추가
             .field("password", .string, .required)  // password 필드 추가
             .create()
