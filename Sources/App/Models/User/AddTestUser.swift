0e98bceb1fa73f611dd6c802f5bb626833434583
kim-jiha95 <kimjiha1112@gmail.com>
2024-10-18 09:47:15 +0900

    Fix password hash

--- Sources/App/Models/User/AddTestUser.swift
+++ Sources/App/Models/User/AddTestUser.swift
@@ -11,13 +11,8 @@ import Crypto
 
 struct AddTestUser: Migration {
     func prepare(on database: Database) -> EventLoopFuture<Void> {
-        // 비밀번호 해시화
         let hashedPassword = try! Bcrypt.hash("password123")
-
-        // 사용자 생성
         let user = User(username: "testuser", email: "testuser@example.com", passwordHash: hashedPassword)
-        
-        // 데이터베이스에 저장
         return user.save(on: database)
     }
 
