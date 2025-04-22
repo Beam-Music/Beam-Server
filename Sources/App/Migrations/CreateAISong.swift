6e0df2d9d840aa816148d9135800b537ca9a25cd
Jiha Kim <kimjiha1112@gmail.com>
2025-04-22 10:52:49 +0900

    fix aisong field

--- Sources/App/Migrations/CreateAISong.swift
+++ Sources/App/Migrations/CreateAISong.swift
@@ -16,7 +16,7 @@ struct CreateAISong: AsyncMigration {
             .field("release_date", .date)
             .field("duration", .int, .required)
             .field("is_ai_generated", .bool, .required)
-            .field("file_path", .string, .required)
+            .field("file_url", .string, .required) 
             .field("song_id", .uuid, .references("songs", "id"))
             .create()
     }
