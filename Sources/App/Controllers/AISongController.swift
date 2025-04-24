77f35652bb7c1574b3f8077c40fbbf3fd04d4e64
Jiha Kim <kimjiha1112@gmail.com>
2025-04-24 09:18:43 +0900

    add dualipa-killbill

--- Sources/App/Controllers/AISongController.swift
+++ Sources/App/Controllers/AISongController.swift
@@ -86,7 +86,7 @@ struct AISongController: RouteCollection {
         try await newSong.save(on: req.db)
         let newSongID = try newSong.requireID()
         
-        let fileUrl = "/ai-songs/\(input.fileName)"
+        let fileUrl = "https://soundcloud.com/beyooyoqhphl/duapila-killbill"
         
         let aiSong = AiSong(
             title: newSong.title,
